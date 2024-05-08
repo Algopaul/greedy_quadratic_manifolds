6a68a2a9e1f9a5db9a0444a7579380257f626ede
Algopaul <72927083+Algopaul@users.noreply.github.com>
2024-05-08 12:55:14 -0400

    add reg magnitude as flag

--- greedyqm/quadmani.py
+++ greedyqm/quadmani.py
@@ -15,6 +15,8 @@ N_VECTORS_TO_CHECK = flags.DEFINE_integer(
     'n_vectors_to_check', 200,
     'Number of vectors to check in the greedy algorithm')
 
+REG_MAGNITUDE = flags.DEFINE_float('reg_magnitude', 1e-6, 'Regularization magnitude for least squares problem')
+
 
 def default_feature_map(reduced_data_points):
   r = reduced_data_points.shape[0]
@@ -26,11 +28,14 @@ def default_feature_map(reduced_data_points):
 def quadmani_greedy(data_points,
                     r=None,
                     n_vectors_to_check=None,
+                    reg_magnitude=None,
                     feature_map=default_feature_map):
   if r is None:
     r = REDUCED_DIMENSION.value
   if n_vectors_to_check is None:
     n_vectors_to_check = N_VECTORS_TO_CHECK.value
+  if reg_magnitude is None:
+    reg_magnitude = REG_MAGNITUDE.value
   shift_value = jnp.mean(data_points, axis=1)
   phi, sigma, psit = jnp.linalg.svd(shift_data(data_points, shift_value))
   idx_in = jnp.arange(0, 0, 1)
@@ -42,6 +47,7 @@ def quadmani_greedy(data_points,
       psit,
       imax=n_vectors_to_check,
       nonlinear_map=feature_map,
+      reg_magnitude=reg_magnitude,
   )
   logging.info('Greedy step 1. Selected column %s', idx_in)
   for i in range(r - 1):
@@ -52,6 +58,7 @@ def quadmani_greedy(data_points,
         psit,
         imax=n_vectors_to_check,
         nonlinear_map=feature_map,
+        reg_magnitude=reg_magnitude,
     )
     logging.info('Greedy step %i. Selected columns %s', i + 2, idx_in)
   V = phi[:, idx_in]
@@ -60,11 +67,13 @@ def quadmani_greedy(data_points,
   embedded_snapshots = jnp.diag(sigma_1) @ V1T
   V2S2 = V2T.T * sigma_2
   H = feature_map(embedded_snapshots)
-  W, _ = lstsq_l2(H.T, V2S2)
+  W, _ = lstsq_l2(H.T, V2S2, reg_magnitude=reg_magnitude)
   return V, phi[:, idx_out] @ W.T, shift_value
 
 
-def lstsq_l2(A, B, reg_magnitude=1e-6):
+def lstsq_l2(A, B, reg_magnitude=None):
+  if reg_magnitude is None:
+    reg_magnitude = REG_MAGNITUDE.value
   phi, sigma, psi_t = jnp.linalg.svd(A, full_matrices=False)
   sinv = sigma / (sigma**2 + reg_magnitude**2)
   x = psi_t.T * sinv @ (phi.T @ B)
@@ -73,13 +82,13 @@ def lstsq_l2(A, B, reg_magnitude=1e-6):
   return x, resid
 
 
-def greedy_step_fast(idx_in_pre, idx_out_pre, sigma, VT, imax, nonlinear_map):
+def greedy_step_fast(idx_in_pre, idx_out_pre, sigma, VT, imax, nonlinear_map, reg_magnitude):
 
   def body_fun(i, errors):
     idx_in = jnp.hstack((idx_in_pre, idx_out_pre[i]))
     idx_out = jnp.delete(idx_consider, i, assume_unique_indices=True)
     errors = errors.at[i].set(
-        compute_error_fast(idx_in, idx_out, sigma, VT, nonlinear_map))
+        compute_error_fast(idx_in, idx_out, sigma, VT, nonlinear_map, reg_magnitude))
     return errors
 
   n_consider = jnp.minimum(imax, len(idx_out_pre))
@@ -92,13 +101,13 @@ def greedy_step_fast(idx_in_pre, idx_out_pre, sigma, VT, imax, nonlinear_map):
   return idx_in_next, idx_out_next
 
 
-def compute_error_fast(idx_in, idx_out, sigma, VT, feature_map):
+def compute_error_fast(idx_in, idx_out, sigma, VT, feature_map, reg_magnitude):
   sigma_1, sigma_2 = sigma[idx_in], sigma[idx_out]
   V1T, V2T = VT[idx_in, :], VT[idx_out, :]
   embedded_snapshots = jnp.diag(sigma_1) @ V1T
   V2S2 = V2T.T * sigma_2
   W = feature_map(embedded_snapshots)
-  _, residuals = lstsq_l2(W.T, V2S2)
+  _, residuals = lstsq_l2(W.T, V2S2, reg_magnitude)
   return residuals
 
 
